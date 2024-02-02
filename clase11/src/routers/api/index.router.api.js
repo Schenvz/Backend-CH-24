d604dd650dc5ddd2e58d9047ed0f8267d0a930b7
Lucas <lucasgori123@gmail.com>
2024-02-02 16:50:04 -0300

    clase 11 1.1

---
+++ clase11/src/routers/api/index.router.api.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import usersRouter from "./users.router.api.js";
+import eventsRouter from "./events.router.api.js";
+
+const apiRouter = Router()
+
+apiRouter.use("/users",usersRouter)
+apiRouter.use("/events",eventsRouter)
+
+export default apiRouter
